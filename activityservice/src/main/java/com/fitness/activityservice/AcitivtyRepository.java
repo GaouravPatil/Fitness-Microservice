ad33740c19393a42ef9b34a1893f4a14a6d0ea36
gaouravpatil <patilgaourav304@gmail.com>
2026-01-01 18:09:37 +0530

    remote main with local repository

--- activityservice/src/main/java/com/fitness/activityservice/AcitivtyRepository.java
+++ activityservice/src/main/java/com/fitness/activityservice/AcitivtyRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AcitivtyRepository extends MongoRepository<Activity,String> {
+
 }
--- activityservice/src/main/java/com/fitness/activityservice/dto/ActivityResponse.java
+++ activityservice/src/main/java/com/fitness/activityservice/dto/ActivityResponse.java
@@ -2,9 +2,6 @@ package com.fitness.activityservice.dto;
 
 import com.fitness.activityservice.model.ActivityType;
 import lombok.Data;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.mongodb.core.mapping.Field;
 
 import java.time.LocalDateTime;
 import java.util.Map;
